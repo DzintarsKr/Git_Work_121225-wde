704caebc2b89a98cf204de086abf25540fc924cb
Dzintars <d.krakops@t-online.de>
2026-04-30 21:49:58 +0200

    script without 4 and 7 Issues

--- filemover.sh
+++ filemover.sh
@@ -5,15 +5,24 @@ read -p "Введите путь к исходной директории (sourc
 read -p "Введите путь к целевой директории (target_directory): " target_directory
 
 # Запрос расширения файлов, которые нужно скопировать
+read -p "Enter the searched file format (txt, jpg, mp3): " file_extension
 
 # Запросить новое расширение для файлов.
+read -p "Введите новое расширение (например txt): " new_ext
 
 # Проверка существования исходной директории и целевой директории
 
 
 # Проверка, есть ли файлы с указанным расширением в исходной директории
+files=$(find "$source_directory" -type f -name "*.$file_extension")
+if [ -z "$files" ]; then
+echo "Error: files with extension .$file_extension not found"
+exit 1
+fi
 
 # Копирование файлов с указанным расширением в целевую директорию
+cp $source_directory/*.$file_extension $target_directory/
+echo "Каждый файл из исходной директории был скопирован в целевую."
 
 # Архивация исходных файлов.
 
